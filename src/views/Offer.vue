61cd5b8d79d9a8693317b773e94dba65a861ef8b
HarryHao <koler778@gmail.com>
2020-06-05 19:49:42 +0800

    添加成绩查询页面，录取查询页面

--- src/views/Offer.vue
+++ src/views/Offer.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    录取查询
+  <div class="offer-container">
+    <el-card>
+      <h3 class="offer-title">录取通知书</h3>
+      <p>
+        <span class="offer-fill">
+          {{ $store.state.user.username }}
+        </span>
+        同学:
+      </p>
+      <p class="offer-par">
+        恭喜你，录取你入我校
+        <span class="offer-fill">xxxx</span>专业学习。
+      </p>
+      <p class="offer-par">具体报道时间、地点，见《新生入学通知》</p>
+    </el-card>
+    <!-- TODO 未被录取的消息 -->
   </div>
 </template>
 
@@ -8,4 +22,20 @@
 export default {}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.offer-container {
+  .offer-title {
+    font-size: 30px;
+    text-align: center;
+  }
+  .offer-fill {
+    text-decoration: underline;
+    font-size: 22px;
+    line-height: 22px;
+    text-underline-position: under;
+  }
+  .offer-par {
+    text-indent: 3em;
+  }
+}
+</style>
--- src/views/Score.vue
+++ src/views/Score.vue
@@ -1,11 +1,71 @@
 <template>
-  <div>
-    score
+  <div class="score-container">
+    <el-card>
+      <h3 style="text-align: center; font-size: 20px;">考试成绩</h3>
+      <el-row :gutter="24" type="flex" justify="center">
+        <el-col
+          v-for="score of scores"
+          :key="score.cname"
+          :span="6"
+          class="score"
+        >
+          <el-progress
+            type="circle"
+            :percentage="(score.grade / score.total) * 100"
+            :color="colors"
+          ></el-progress>
+          <p class="course-name">{{ score.cname }}</p>
+          <p class="course-name">{{ score.grade + '分' }}</p>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      scores: [
+        {
+          cname: '数据结构',
+          grade: 50,
+          total: 100
+        },
+        {
+          cname: 'C语言',
+          grade: 75,
+          total: 100
+        },
+        {
+          cname: '英语',
+          grade: 91,
+          total: 100
+        },
+        {
+          cname: '总分',
+          grade: 216,
+          total: 300
+        }
+      ],
+      colors: [
+        { color: '#f56c6c', percentage: 59 },
+        { color: '#5cb87a', percentage: 75 },
+        { color: '#6f7ad3', percentage: 90 }
+      ]
+    }
+  },
+  methods: {}
+}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.score-container {
+  .score {
+    text-align: center;
+  }
+  .course-name {
+    text-align: center;
+  }
+}
+</style>
