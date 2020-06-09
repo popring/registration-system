fc3e08b1cd0b2c1322c7f31ef17c50a89754e571
HarryHao <koler778@gmail.com>
2020-06-09 13:01:09 +0800

    修改登录样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,57 +1,64 @@
 <template>
   <div class="page-container">
     <div class="logo"></div>
-    <div class="motto"></div>
-    <el-card class="login-card">
-      <el-form class="login-form">
-        <div class="title-container">
-          <h3>专升本报名系统</h3>
-        </div>
-        <el-form-item>
-          <el-input
-            v-model="user.username"
-            placeholder="用户名/手机号"
-            prefix-icon="el-icon-user"
-          />
-        </el-form-item>
-        <el-form-item>
-          <el-input
-            v-model="user.userpwd"
-            show-password
-            placeholder="密码"
-            prefix-icon="el-icon-lock"
-          />
-        </el-form-item>
-        <el-form-item>
-          <el-select
-            v-model="user.role"
-            placeholder="请选择角色"
-            @change="handleSwitch"
-            style="width: 100%;"
-          >
-            <el-option label="学生" value="student"></el-option>
-            <el-option label="管理员" value="admin"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="handleLogin" class="btn-login"
-            >登录</el-button
-          >
-        </el-form-item>
-        <el-form-item>
-          <router-link to="/signup" v-slot="{ href }">
-            <span class="signup">
-              没有账号?
-              <a :href="href">点我注册</a>
-            </span>
-          </router-link>
-        </el-form-item>
-      </el-form>
-    </el-card>
+    <el-row type="flex" justify="space-around">
+      <el-col :md="8" class="hidden-sm-only hidden-xs-only hidden-md-only">
+        <div class="motto"></div>
+      </el-col>
+      <el-col :xs="20" :sm="20" :md="20" :lg="6">
+        <el-card class="login-card">
+          <el-form class="login-form">
+            <div class="title-container">
+              <h3>专升本报名系统</h3>
+            </div>
+            <el-form-item>
+              <el-input
+                v-model="user.username"
+                placeholder="用户名/手机号"
+                prefix-icon="el-icon-user"
+              />
+            </el-form-item>
+            <el-form-item>
+              <el-input
+                v-model="user.userpwd"
+                show-password
+                placeholder="密码"
+                prefix-icon="el-icon-lock"
+              />
+            </el-form-item>
+            <el-form-item>
+              <el-select
+                v-model="user.role"
+                placeholder="请选择角色"
+                @change="handleSwitch"
+                style="width: 100%;"
+              >
+                <el-option label="学生" value="student"></el-option>
+                <el-option label="管理员" value="admin"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="handleLogin" class="btn-login"
+                >登录</el-button
+              >
+            </el-form-item>
+            <el-form-item>
+              <router-link to="/signup" v-slot="{ href }">
+                <span class="signup">
+                  没有账号?
+                  <a :href="href">点我注册</a>
+                </span>
+              </router-link>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import 'element-ui/lib/theme-chalk/display.css'
 import { login } from '@/api/'
 export default {
   data() {
@@ -110,7 +117,7 @@ export default {
   min-height: 100%;
   background: url(../assets/login-bg.png) no-repeat;
   background-size: cover;
-  padding: 50px 0 0 50px;
+  padding: 50px 0 0;
   box-sizing: border-box;
 
   .logo {
@@ -120,19 +127,18 @@ export default {
   }
 
   .motto {
-    float: left;
-    width: 300px;
-    height: 300px;
+    // float: left;
+    width: 100%;
+    height: 600px;
     background: url(../assets/motto.png) no-repeat;
-    background-size: contain;
-    transform: scale(2.5) translate(50%, 35%);
+    background-size: 100%;
   }
 
   .login-card {
-    float: right;
-    transform: translate(-60%, 30%);
+    // float: right;
+    // transform: translate(-60%, 30%);
     position: relative;
-    width: 520px;
+    // width: 520px;
     box-sizing: border-box;
 
     .login-form {
